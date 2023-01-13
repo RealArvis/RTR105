e0537f0cb69b0c2a7fefc7c5ff2df940c489079e
RealArvis <112925729+RealArvis@users.noreply.github.com>
2023-01-13 04:57:37 +0200

    Update while+factorial.c

--- while+factorial/while+factorial.c
+++ while+factorial/while+factorial.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    int userNum;
+    char userType;
+    printf("Enter a decimal number: ");
+    scanf_s("%d", &userNum);
+    printf("Enter data type to use (c for char, i for int, ll for long long): ");
+    scanf_s(" %c", &userType);
+
+    long long factorial = 1;
+    int i = userNum;
+    while (i > 1) {
+        if (userType == 'c' && i > 5) {
+            printf("Cannot calculate factorial using char data type, value would be too large.\n");
+            return 0;
+        }
+        else if (userType == 'i' && i > 12) {
+            printf("Cannot calculate factorial using int data type, value would be too large.\n");
+            return 0;
+        }
+        else if (userType == 'll' && i > 20) {
+            printf("Cannot calculate factorial using long long data type, value would be too large.\n");
+            return 0;
+        }
+        factorial *= i;
+        i--;
+    }
+    printf("Factorial of %d is: %lld\n", userNum, factorial);
+    return 0;
+}
